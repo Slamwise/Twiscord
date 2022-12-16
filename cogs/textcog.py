d7ffd35e7b27d5ded11be539a49fabc95b06fcd1
slamwise <smsa2222@yahoo.com>
2022-12-16 17:01:52 -0500

    testing

--- cogs/textcog.py
+++ cogs/textcog.py
@@ -28,6 +28,7 @@ class Texts(commands.Cog):
 
     @tasks.loop(seconds=0.5)
     async def check_tweets(self):
+        print(self.subsconfig)
         for handle in shared_tweets:
             if handle not in self.subsconfig:
                 continue
