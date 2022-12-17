cc76151e91a18fdc4da1d9fd0b8ded57ca53da22
Kevin Thapa <imbesci@gmail.com>
2022-12-17 15:22:46 -0500

    fix blocker

--- cogs/tweetcog.py
+++ cogs/tweetcog.py
@@ -82,6 +82,7 @@ class Tweets(commands.Cog):
             self.recency_queue = self.recency_queue.union(recent_tweets)
             [shared_tweets[tweet[1]].add(tweet) for tweet in recent_tweets]
             self.count += 1
+            print(self.count)
 
         else:  # first fetch tweet.created_at, tweet.author.screen_name.strip().lower(), tweet.id, tweet.full_text
             self.recency_queue = OrderedDequeSet(fresh_tweets, maxlen=200)
